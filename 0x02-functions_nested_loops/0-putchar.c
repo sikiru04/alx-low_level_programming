e13181e64420533395ff97d1573333faf47af0d2
Sikiru <sikiru04@gmail.com>
2022-09-13 06:44:03 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "sikiru"
+
+/**
+ * main - Prints "Sikiru" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_putchar('S');
+	_putchar('i');
+	_putchar('k');
+	_putchar('i');
+	_putchar('r');
+	_putchar('u');
+	_putchar('\u');
+
+	return (0);
+}
