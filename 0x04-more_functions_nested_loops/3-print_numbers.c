73cc1a1ac709793b69bc2af1b8db3132a119589e
Sikiru <sikiru04@gmail.com>
2022-09-15 18:44:32 -0700

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,20 +2,14 @@
 #include "main.h"
 
 /**
-*print_most_numbers - prints numbers except 2 and 4
-*
-*Return: returns nothing
-*/
-
-void print_most_numbers(void)
+ * print_numbers - Prints the numbers from 0-9.
+ */
+void print_numbers(void)
 {
-	int n;
+	int num;
 
-	for  (n = 48; n < 58; n++)
-	{
-		if ((n == 50) || (n == 52))
-		{
-			continue;
-		}
-		putchar(n);
-	}
+	for (num = 0; num <= 9; num++)
+		_putchar((num % 10) + '0');
+
+	_putchar('\n');
+}
